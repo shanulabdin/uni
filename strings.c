edf92b2f24e9c9105898f95e69c1309a3a559d70
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-07 03:19:24 +0500

    copilot

--- strings.c
+++ strings.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
 
-char* string(){
-  char* str = "This is a String!";
-  return str;
-}
-
-int number(){
+int main()
+{
+  char str[18] = "This is a String!";
   int num = 10;
-  return num;
-}
-
-float decimal(){
   float dec = 3.14;
-  return dec;
-}
-
-int main(){
-  char* str = string();
-  int num = number();
-  float dec = decimal();
 
   printf("%s\n", str);
-  printf("This is an integer: %d\n", num);
-  printf("This is a float: %f\n", dec);
-
+  printf("%d\n", num);
+  printf("%f\n", dec);
   return 0;
 }
