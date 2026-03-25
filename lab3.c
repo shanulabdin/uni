2a211c10cee8f07d1d94d8349915f9ba280d9edb
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-25 18:51:47 +0500

    task 2

--- lab3.c
+++ lab3.c
@@ -38,3 +38,27 @@ int main()
 
   return 0;
 }
+int main() {
+    float marks;
+    printf("Enter your marks: \t");
+    scanf("%f", &marks);
+    
+    if (marks >= 90)
+    {
+        printf("Grade A");
+    } else if (marks >= 80)
+    {
+        printf("Grade B");
+    } else if (marks >= 70)
+    {
+        printf("Grade C");
+    } else if (marks >= 60)
+    {
+        printf("Grade D");
+    } else if (marks < 60)
+    {
+        printf("Grade F");
+    }
+
+    return 0;
+}
