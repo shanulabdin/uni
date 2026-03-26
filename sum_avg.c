ecab4cf3db359d1d5e1f9798793c15990838f4a5
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-26 12:00:00 +0600

    learning c

--- sum_avg.c
+++ sum_avg.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    // Write a C program to take three numbers as input and calculate their sum and average.
-    
     int num1, num2, num3;
     
     printf("Enter number 1:\t");
--- theory3.c
+++ theory3.c
@@ -16,22 +16,3 @@ int main()
 	return 0;
 }
 
-#include <stdio.h>
-#include <ctype.h>
-
-int main() {
-    char guess;
-    printf("Guess the first letter: ");
-    scanf("%c", &guess);
-    
-    if(toupper(guess) == 'N')
-    {
-        printf("Right guess.");   
-    } 
-    else 
-    {
-        printf("Wrong guess.");  
-    }
-
-    return 0;
-}
