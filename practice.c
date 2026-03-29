845e0427c56856c64071227d31106fb27ea345d5
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-29 23:13:40 +0500

    ternary operators in C by heart

--- practice.c
+++ practice.c
@@ -10,7 +10,7 @@ int main()
   printf("Enter 2nd Number: ");
   scanf("%d", &b);
 
-  printf("%d is %s and %d is %s\n", a, (a %2==0)? "Even":"Odd", b, (b %2==0)? "Even":"Odd");
+  printf("%d is %s and %d is %s\n", a, (a %2==0)? "Even":"Odd", b, (b %2==0)?"Even":"Odd");
 
   return 0;
 }
