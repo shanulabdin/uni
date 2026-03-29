4500564cecdc87d4f500aae1b7df024b28f9f3c4
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-29 22:59:55 +0500

    multiple number checks

--- practice.c
+++ practice.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
 int main (){
-  int a;
+  int a, b;
   
-  printf("Enter a Number: ");
+  printf("Enter 1st Number: ");
   scanf("%d", &a);
 
-  if(a %2== 0)
+  printf("Enter 2nd Number: ");
+  scanf("%d", &b);
+
+  if(a %2==0 && b %2==0)
   {
-    printf("%d is an Even Number", a);
-  } else 
+    printf("%d, %d are both Even Numbers\n", a, b);
+  } else if (a %2!=0 && b %2==0)
   {
-    printf("%d is an Odd Number", a);
+    printf("%d is Odd and %d is Even\n", a, b);
+  } else if (a %2==0 && b %2!=0)
+  {
+    printf("%d is Even and %d is Odd\n", a, b);
+  } else {
+    printf("%d, %d are both Odd Numbers\n", a, b);
   }
 
   return 0;
