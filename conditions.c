897e892af5706f6933ddf0f0d5a43dfeec61820a
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-31 22:51:01 +0500

    new version by heart

--- conditions.c
+++ conditions.c
@@ -5,29 +5,18 @@ int main() {
     char letter;
     char lower_letter;
 
-    printf("Write a letter: ");
+    printf("Enter a Letter: ");
     scanf("%c", &letter);
 
     lower_letter = tolower(letter);
-    
+
     switch(lower_letter){
-        case 'a':
-            printf("%c is a Vowel.", letter);
-            break;
-        case 'e':
-            printf("%c is a Vowel.", letter);
-            break;
-        case 'i':
-            printf("%c is a Vowel.", letter);
-            break;
-        case 'o':
-            printf("%c is a Vowel.", letter);
-            break;
-        case 'u':
-            printf("%c is a Vowel.", letter);
-            break;
-        default:
-            printf("%c is a Consonant.", letter);
+      case 'a': printf("%c is a Vowel.", letter); break;
+      case 'e': printf("%c is a Vowel.", letter); break;
+      case 'i': printf("%c is a Vowel.", letter); break;
+      case 'o': printf("%c is a Vowel.", letter); break;
+      case 'u': printf("%c is a Vowel.", letter); break;
+      default: printf("%c is a Consonent.", letter);
     }
     return 0;
 }
