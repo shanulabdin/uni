963ff62d2832b10d15b3191964436b031407e2d0
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-03-31 22:56:48 +0500

    my version

--- conditions.c
+++ conditions.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main() {
-    char letter;
-    char lower_letter;
+int main() 
+{
+  char letter;
+  char lower_letter;
 
-    printf("Enter a Letter: ");
-    scanf("%c", &letter);
+  printf("Enter a Letter: ");
+  scanf("%c", &letter);
 
-    lower_letter = tolower(letter);
+  lower_letter = tolower(letter);
 
-    switch(lower_letter){
-      case 'a': printf("%c is a Vowel.", letter); break;
-      case 'e': printf("%c is a Vowel.", letter); break;
-      case 'i': printf("%c is a Vowel.", letter); break;
-      case 'o': printf("%c is a Vowel.", letter); break;
-      case 'u': printf("%c is a Vowel.", letter); break;
-      default: printf("%c is a Consonent.", letter);
-    }
-    return 0;
+  switch(lower_letter){
+    case 'a': printf("%c is a Vowel.", letter); break;
+    case 'e': printf("%c is a Vowel.", letter); break;
+    case 'i': printf("%c is a Vowel.", letter); break;
+    case 'o': printf("%c is a Vowel.", letter); break;
+    case 'u': printf("%c is a Vowel.", letter); break;
+    
+    default: printf("%c is a Consonant.", letter);
+  }
+  return 0;
 }
