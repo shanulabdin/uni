a32642549d9c1b69b2bc8921e0f91702e41e58a9
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-04-01 12:21:10 +0500

    learning switch case

---
+++ theory4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int choice = 9;
+
+  switch(choice)
+  {
+    case 1: printf("The Choice is 1");
+      break;
+    case 2: printf("The Choice is 2");
+      break;
+    case 3: printf("The Choice is 3");
+      break;
+    default: printf("Not matched value.");
+  }
+
+  return 0;
+}
