c31b6f91c3c11f62181409f9d094dad20a93f4c0
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-04-01 18:45:47 +0500

    my version of solution of conversions

---
+++ lab-4/task_1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+  int choice;
+  float input, converted;
+
+  printf("1. For Kilometers to Miles, press 1: \n");
+  printf("2. Meters to Feet, Press 2: \n");
+  printf("3. Centimeters to Inches, Press 3: \n");
+  printf("Choose conversion type: ");
+
+  scanf("%d", &choice);
+
+  switch (choice)
+  {
+  case 1:
+    printf("User chose Kilometers to Miles: 1: \n");
+    printf("enter your value in Kilometers: ");
+    scanf("%f", &input);
+
+    converted = input * 0.621371;
+
+    printf("%0.2f Kilometers is equal to %0.2f Miles.\n", input, converted);
+    break;
+  case 2:
+    printf("User chose Meters to Feet: 2: \n");
+    printf("enter your value in Meters: ");
+    scanf("%f", &input);
+
+    converted = input * 3.28084;
+    printf("%0.2f Meters is equal to %0.2f Feet.\n", input, converted);
+    break;
+  case 3:
+    printf("Centimeters to Inches: 3: \n");
+    printf("enter your value in Centimeters: ");
+    scanf("%f", &input);
+
+    converted = input * 0.393701;
+    printf("%0.2f Centimeters is equal to %0.2f Inches.\n", input, converted);
+    break;
+  default:
+    printf("invalid input \n");
+  }
+  return 0;
+}
