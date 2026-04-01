f8f36d0b473af4ca6c681c5bf06910a01cd93a42
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-04-01 14:27:23 +0500

    writing a loop for summing nums

--- Theory-4/loops/task_1.c
+++ Theory-4/loops/task_1.c
@@ -2,10 +2,15 @@
 
 int main()
 {
-  int i;
-  for (i=1; i <= 10; i++)
+  int i, num=0, sum = 0;
+  for (i = 1; i <= 10; i++)
   {
-    printf("%d\n", i);
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    sum = sum + num;
+    printf("Current sum: %d\n", sum);
   }
+  printf("Final sum: %d\n", sum);
   return 0;
 }
