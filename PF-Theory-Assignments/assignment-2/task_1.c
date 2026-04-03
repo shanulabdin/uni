cac0b3816b2352d6d0798422883144f9b09907dc
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-04-03 13:49:33 +0500

    PF theory assignment 2

---
+++ PF-Theory-Assignments/assignment-2/task_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+  int num;
+
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  if (num % 3 == 0 && num % 5 == 0)
+  {
+    printf("FizzBuzz");
+  }
+  else if (num % 3 == 0)
+  {
+    printf("Fizz");
+  }
+  else if (num % 5 == 0)
+  {
+    printf("Buzz");
+  }
+  else
+  {
+    printf("Not FizzBuzz");
+  }
+
+  return 0;
+}
