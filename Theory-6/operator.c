fa23b25245a555e949eedacbda21da60a5f2d7e2
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-04-22 14:30:36 +0500

    operators

---
+++ Theory-6/operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+  // int a = 5, b = 5;
+  // int result = a == b;
+
+  // printf("%d ", result);
+
+  // int a = 5, b = 6;
+  // int result = (a>3)&&(b>5);
+
+  // printf("%d ", result);
+
+  // int a = 5, b = 2;
+  // int result = (a > 3) || (b > 5);
+
+  // printf("%d ", result);
+
+  int a = 1, b = 2;
+  int result = !(a > b);
+
+  printf("%d ", result);
+  return 0;
+}
