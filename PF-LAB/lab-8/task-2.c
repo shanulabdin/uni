aa7026850b78dd8bb080388bd4935182617efeb5
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-05-11 18:44:08 +0500

    task 2 complete

--- PF-LAB/lab-8/task-2.c
+++ PF-LAB/lab-8/task-2.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
 int main(){
-  int 
+  int arr1[100] = {};
+  int len = 0;
 
+  printf("How many numbers do you want to enter? No: ");
+  scanf("%d", &len);
+  
+  for(int i=0; i<len; i++){
+    printf("Enter Number %d: ", i+1);
+    scanf("%d", &arr1[i]);
+  }
+
+  printf("\nArray Elements Reversed: ");
+  for(int i=len-1; i>=0; i--){
+    printf("%d ", arr1[i]);
+  }
   return 0;
 }
 
-
 // 2.	Write a C program that takes an array of integers as input and outputs the array in reverse order. The program should work for arrays of any size up to 100 elements.
-// 3.	Write a C program that takes an array of integers and its size as inputs, and sorts the array in ascending order using the Bubble Sort algorithm. Display the sorted array in the main function.
-// , create 3 new files inside lab-8, use only 
