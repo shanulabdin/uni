bb137986209ce8d4f16f018e9bbd99864c638c43
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-06-02 23:30:00 +0500

    practice 2

--- Theory-10/task-2.c
+++ Theory-10/task-2.c
@@ -4,6 +4,7 @@ int sum(int n) {
   if(n == 1) return 1;
   return n + sum(n - 1);
 }
+
 int main() {
   printf("%d", sum(5));
 
---
+++ Theory-10/task-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if(n == 0) return 1;
+  return n * factorial(n - 1);
+}
+int main() {
+  factorial(5);
+
+  return 0;
+}
