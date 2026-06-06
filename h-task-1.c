1d0a12fb3aeb605ed869bb8fbf8681f09a817d43
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-06-06 23:55:08 +0500

    delivery

---
+++ h-task-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int calDel(int);
+
+int main(){
+  int order = 0, delivery = 0, total = 0;
+
+  printf("Enter total Bill: ");
+  scanf("%d", &order);
+
+  delivery = calDel(order);
+  total = delivery + order;
+
+  printf("Your Delivery charges will be: %d\n", delivery);
+  printf("Total: %d", total);
+
+  return 0;
+}
+
+int calDel(int order){
+  int delivery = order > 1000 ? 150 : 0;
+  
+  return delivery;
+}
