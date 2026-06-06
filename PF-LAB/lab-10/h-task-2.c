738fe7f3712200970eab509afe61214853fe2a37
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-06-07 02:06:03 +0500

    task2

---
+++ PF-LAB/lab-10/h-task-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[], int left, int right)
+{
+    if (left >= right)
+        return;
+
+    // swap characters
+    char temp = str[left];
+    str[left] = str[right];
+    str[right] = temp;
+
+    // recursive call
+    reverseString(str, left + 1, right - 1);
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    reverseString(str, 0, len - 1);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
